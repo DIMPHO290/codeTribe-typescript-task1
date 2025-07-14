373114241932d805ace2b067b1c1cad059216728
DIMPHO290 <dimphotshidi1@gmail.com>
2025-07-14 21:30:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,32 @@
-// const greet = (name : String , age: number , surname : String , isOld : boolean) : string =>
-//   {
-//     if(isOld ===false){
-//        return `Hello,${name}, welcome to TS CodeTribe and ${name} 's age is ${age} and her surname is ${surname}`
-//     }else{
-//        console.log("start again")
-//     }
-// }
-// console.log(greet("Dimpho" , 23 ,"Mahwibila" , false));
-"use strict";
-const greet = (name) => {
-    return `${name}`;
+const employee1 = {
+  fullName: ["Justin", "Mahwibila"],
+  age: 25,
+  department: "ICT",
+  skills: ["HTML", "Java", "Coding"],
+  isActive: true,
 };
-console.log(greet("Dimpho"));
-//Basic Type
-//Number
-let age = 25;
-let price = 135.8;
-let hex = 0xff;
-//String
-let firstName = "Dimpho";
-let greeting = `Hello ,${firstName}`;
-//Boolean
-let isLoggedIn = true;
-let hasPermission = false;
-//Example
-const showBoolean = (name) => {
-    return `${name}`;
+
+const employee2 = {
+  fullName: ["Khumo", "Mokoena"],
+  age: 19,
+  department: "Education",
+  skills: ["listening", "Math", "Teaching"],
+  isActive: false,
 };
-console.log(showBoolean(isLoggedIn));
-//Array
-//The ways to define arrays
-//first
-let numbers = [1, 2, 3, 4];
-let fruits = ["Apples", "Banana", "Orange"];
-//Example
-const showArray = (name) => {
-    return name;
+
+const logEmployee = (employee) => {
+  console.log('---Employee Details---');
+  Object.entries(employee).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      console.log(`${key} : ${value.join(" ")}`);
+    } else if (typeof value === "boolean") {
+      console.log(`${key} : ${value ? "Yes" : "No"}`);
+    } else {
+      console.log(`${key} : ${value}`);
+    }
+  });
+  console.log('----------------------');
 };
-console.log(showArray(numbers));
-//second way
-let scores = [100, 10, 25];
-//Tuple
-//Arrays with fixed length and types in specific postion
-let user = ["Dimpho", 23];
-const showTuple = (name) => {
-    return name;
-};
-console.log(showTuple(user));
-//object
-//Basic Object type
-let person = { firstName: "Dimpho", age: 23 };
-const showObject = (name) => {
-    return name;
-};
-console.log("My name is ", person.firstName);
-console.log(" Age ", person.age);
-let studentTut = { name: "Dimpho", score: 56 };
-function displayStudent(student) {
-    console.log(student.name);
-}
-displayStudent(studentTut);
-let myCar = { brand: "bmw", year: 2023, isNew: true };
-function displayCarBrand(car) {
-    console.log(car.brand);
-}
-displayCarBrand(myCar);
+
+logEmployee(employee1);
+logEmployee(employee2);
